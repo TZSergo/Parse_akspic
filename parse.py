80ef0028fc6dcc9cce56830f8c786512f8798b60
TZSergo <sergozero90@gmail.com>
2022-04-09 02:13:51 +0300

    get links

--- parse.py
+++ parse.py
@@ -1,1 +1,16 @@
-print("Hello world!")
+import requests
+from bs4 import BeautifulSoup
+
+
+url = 'https://wallpaperscraft.ru/all/3840x2160/page2'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'lxml')
+quotes = soup.find_all('a', class_='wallpapers__link')
+
+#print(quotes)
+
+for quote in quotes:
+    print(quote.get('href'))
+    #print(quote.text)
+
+print("The END!")
