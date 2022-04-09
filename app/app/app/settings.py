ab0f9538e26043f57630660c6ef3fc9576d613f1
TZSergo <sergozero90@gmail.com>
2022-04-10 02:42:17 +0300

    settings django

--- app/app/app/settings.py
+++ app/app/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@+(f#3pg8wk%s9mo(b98t20wwq#jip)ua47g37hyxx+y$$^o8('
+SECRET_KEY = 'django-insecure-@+(f#3pg8wtk%s9mo(yb98t20wwq#jip)ua47g37hyxx+y$$^o8('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main'
 ]
 
 MIDDLEWARE = [
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
